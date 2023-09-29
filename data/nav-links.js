de2a1aebf25b1effa6c13823953282bddc03ee69
Zakusei <zakusei.dev@gmail.com>
2023-09-29 23:30:53 +0800

    feat(data): add nav links data

---
+++ data/nav-links.js
@@ -0,0 +1,22 @@
+export const navLinks = [
+  {
+    title: "Why Vintazk?",
+    href: "/",
+  },
+  {
+    title: "Services",
+    href: "/",
+  },
+  {
+    title: "FAQs",
+    href: "/",
+  },
+  {
+    title: "Blogs",
+    href: "/",
+  },
+  {
+    title: "About Us",
+    href: "/about-us",
+  },
+];
