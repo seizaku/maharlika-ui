fb374e03f630dff09af9f902ce20a4b3c8a007e8
Zakusei <zakusei.dev@gmail.com>
2023-10-01 03:21:37 +0800

    feat(navbar): fix style navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,15 +1,77 @@
+"use client";
+
 import Image from "next/image";
-import { MenuIcon } from "lucide-react";
+import { AlignRightIcon, AlignJustify } from "lucide-react";
 import { Button, buttonVariants } from "./ui/button";
 import { navLinks } from "@/data/nav-links";
 import Link from "next/link";
+import { useState } from "react";
+import { Separator } from "@/components/ui/separator";
 
 export default function Navbar() {
+  const [mobileNavState, setState] = useState(false);
   return (
-    <header className="sticky z-40 top-0 left-0 bg-background w-full shadow-md">
-      <nav className="p-2 flex justify-between md:justify-evenly items-center">
-        <Image src={"/logo.png"} width={192} height={64} alt="navbar-logo" />
-        <ul className="hidden md:flex gap-6">
+    <header className="sticky z-50 top-0 left-0 bg-background w-full shadow-md">
+      <nav
+        className={`${
+          mobileNavState && "h-screen"
+        } md:h-auto py-2 px-6 flex flex-col justify-between md:flex-row md:justify-evenly items-center`}
+      >
+        <div className="w-full md:w-auto">
+          <div className="w-full flex justify-between items-center">
+            <Image
+              src={"/logo.png"}
+              width={164}
+              height={64}
+              className="block md:hidden"
+              alt="navbar-logo"
+            />
+            <Image
+              src={"/logo.png"}
+              width={240}
+              height={64}
+              className="hidden md:block"
+              alt="navbar-logo"
+            />
+            <Button
+              onClick={() => setState((prev) => !prev)}
+              className="md:hidden"
+              size="icon"
+              variant="link"
+            >
+              {!mobileNavState && (
+                <AlignJustify className="text-2xl animate-fade text-foreground h-8 w-8 order-2" />
+              )}
+              {mobileNavState && (
+                <AlignRightIcon className="text-2xl text-foreground h-8 w-8 order-2" />
+              )}
+            </Button>
+          </div>
+          {/* Mobile Nav */}
+          <ul
+            className={`${
+              mobileNavState ? "flex" : "hidden"
+            } md:hidden w-full text-lg font-light md:w-auto px-1 mt-10 md:mt-0 md:p-0 flex-col md:flex-row gap-6`}
+          >
+            {navLinks.map(({ title, href, children }, index) => (
+              <li key={index}>
+                {!children && (
+                  <Link className="hover:text-accent-foreground/80" href={href}>
+                    {title}
+                  </Link>
+                )}
+                <Separator className="mt-2" />
+                {children && (
+                  <Link className="hover:text-accent-foreground/80" href="#">
+                    {title}
+                  </Link>
+                )}
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/* Desktop Nav */}
+        <ul className="hidden md:flex w-auto gap-6">
           {navLinks.map(({ title, href, children }, index) => (
             <li key={index}>
               {!children && (
@@ -31,12 +93,20 @@ export default function Navbar() {
             </li>
           ))}
         </ul>
-        <div className="flex gap-4 items-center">
-          <Button size="lg" className="font-light">
-            Contact Us
+        <div
+          className={`${
+            mobileNavState ? "flex" : "hidden md:flex"
+          } w-full md:w-auto flex-col md:flex-row gap-2 mb-4 md:mb-0`}
+        >
+          <Button
+            variant="ghost"
+            size="lg"
+            className="w-full md:w-auto font-light"
+          >
+            Login
           </Button>
-          <Button className="md:hidden" size="sm" variant="ghost">
-            <MenuIcon className="text-2xl" />
+          <Button size="lg" className="w-full md:w-auto font-light">
+            Contact Us
           </Button>
         </div>
       </nav>
