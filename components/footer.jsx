ef61563cb5ff7aa51c38a8ea3f82223bb1c6c6f3
Zakusei <zakusei.dev@gmail.com>
2023-10-01 03:21:08 +0800

    feat(footer): add footer

--- components/footer.jsx
+++ components/footer.jsx
@@ -10,30 +10,30 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="w-screen text-center h-fit bg-slate-900 p-8 px-44 text-slate-50">
+    <footer className="w-screen text-center h-fit bg-slate-900 p-8 md:px-44 text-slate-50">
       <div className="flex justify-between items-center">
-        <p className="text-muted-foreground">
+        <p className="text-muted-foreground text-xs md:text-sm">
           © Zakusei 2023. All Rights Reserved.
         </p>
         <ul className="flex gap-8 text-slate-500">
           <li>
             <Link href="#">
-              <LinkedinIcon />
+              <LinkedinIcon className="h-4 w-4 md:h-auto md:w-auto" />
             </Link>
           </li>
           <li>
             <Link href="#">
-              <FacebookIcon />
+              <FacebookIcon className="h-4 w-4 md:h-auto md:w-auto" />
             </Link>
           </li>
           <li>
             <Link href="#">
-              <YoutubeIcon />
+              <YoutubeIcon className="h-4 w-4 md:h-auto md:w-auto" />
             </Link>
           </li>
           <li>
             <Link href="#">
-              <InstagramIcon />
+              <InstagramIcon className="h-4 w-4 md:h-auto md:w-auto" />
             </Link>
           </li>
         </ul>
